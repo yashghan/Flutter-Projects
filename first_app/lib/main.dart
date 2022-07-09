0960bdfa4b3d22064bf41583eda63b992005dcec
yashghan <64274477+yashghan@users.noreply.github.com>
2022-07-09 21:23:45 +0530

    Update main.dart

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,3 +1,7 @@
+// flutter create flutter_project_name
+// cd flutter_project_name  
+// flutter run
+// flutter packages get
 import 'package:flutter/material.dart';
 
 void main() {
@@ -22,7 +26,7 @@ class MyApp extends StatelessWidget {
         // or simply save your changes to "hot reload" in a Flutter IDE).
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
-        primarySwatch: Colors.blue,
+        primarySwatch: Colo  rs.blue,
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
